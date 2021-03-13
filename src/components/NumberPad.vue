79dccd06000689da3239868fff9080dd52ed0eed
Suen <1467820831@qq.com>
2021-03-13 17:31:21 +0800

    第一次布局Accounting界面

---
+++ src/components/NumberPad.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="numberPad">
+<div class="output">
+    <label for="remarks">备注：</label>
+    <input id="remarks" type="text"/>
+</div>
+<div class="buttons">
+    <button>1</button>
+    <button>4</button>
+    <button>7</button>
+    <button>.</button>
+    <button>2</button>
+    <button>5</button>
+    <button>8</button>
+    <button>0</button>
+    <button>3</button>
+    <button>6</button>
+    <button>9</button>
+    <button class="delete">x</button>
+    <button class="addnumber">+</button>
+    <button class="jiannumber">-</button>
+    <button class="ok">OK</button>
+</div>
+    </div>
+</template>
+
+<script lang="ts">
+
+export default{
+    name: 'NumberPad'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+.numberPad {
+    width: 100%;
+    @keyframes upscoll {
+        0%{
+            transform:translateY(304px)
+        }
+        100%{
+            transform:translateY(0px)
+        }
+    }
+      position: absolute;
+      bottom: 0;
+      height: 304px;
+      animation: upscoll 0.6s;
+    .output {
+      @extend %clearFix;
+      @extend %innerShadow;
+      font-size: 26px;
+      line-height: 36px;
+      font-family: Consolas, monospace;
+      padding: 6px 20px;
+      text-align: left;
+      height: 48px;
+      background-image: linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%);
+
+    }
+    .buttons {
+      background-color:#F2F2F4;
+      width: 100%;
+      height: 256px;
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      align-content: space-evenly;
+      padding: 8px 0 16px 0;
+        button {
+        width: 22%;
+        height: 50px;
+        float: left;
+      background-color:#FDFDFE;
+        border-radius:27px;
+        border: none;
+        font-size: 18px;
+        font-weight: bolder;
+        margin-top: 8px;
+        &.ok {
+          flex-grow: 1;
+          border-radius: 500px;
+        }
+        &.zero {
+          width: 25*2%;
+        }
+      }
+    }
+  }
+</style>
--- src/views/Accounting.vue
+++ src/views/Accounting.vue
@@ -1,17 +1,90 @@
 <template>
     <div>
-<Layout>
-        Accounting
-      </Layout>
+        <header class="header">
+          <div class="title">
+
+          <a href="#/bill" class="back">ab</a>
+          <span>记一笔</span>
+          </div>
+        </header>
+        <ul class="itemwrapper">
+          <li>
+            <icon iconid="daily"/>
+            <div>日常</div>
+          </li>
+          <li>
+            <icon iconid="restaurant"/>
+            <div>餐饮</div>
+          </li>
+          <li>
+            <icon iconid="shopping"/>
+            <div>购物</div>
+          </li>
+          <li>
+            <icon iconid="traffic"/>
+            <div>交通</div>
+          </li>
+          <li>
+            <icon iconid="entertainment"/>
+            <div>娱乐</div>
+          </li>
+          <li>
+            <icon iconid="learn"/>
+            <div>学习</div>
+          </li>
+          <li>
+            <icon iconid="sport"/>
+            <div>运动</div>
+          </li>
+          <li>
+            <icon iconid="house"/>
+            <div>住房</div>
+          </li>
+          <li>
+            <icon iconid="watele"/>
+            <div>水电网</div>
+          </li>
+          <li>
+            <icon iconid="medical"/>
+            <div>医药</div>
+          </li>
+        </ul>
+        <NumberPad/>
     </div>
 </template>
 
 <script lang='ts'>
+import NumberPad from "@/components/NumberPad.vue"
 export default {
+  components: { NumberPad },
   name: 'Accounting'
 }
 </script>
 
 <style lang="scss" scoped>
-
+.header{
+  width: 100%;
+  height: 80px;
+  background-image: linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%);
+  text-align: center;
+  .back{
+    position: absolute;
+    left: 20px;
+  }
+  
+}
+.itemwrapper{
+    display: flex;
+    flex-wrap: wrap;
+    li{
+      width: 20%;
+      text-align: center;
+      list-style: none;
+      padding-top: 14px;
+      font-size: 12px;
+      .icon{
+        font-size: 42px;
+      }
+    }
+  }
 </style>
